8c89528b13ee22943627077f6b4e85c09a7109a5
Kripton <kripton@iMac-Kripton.lan>
2022-10-28 20:24:36 +0300

    view mini refactor

--- src/view.js
+++ src/view.js
@@ -1,20 +1,19 @@
 const render = (elements, i18nextInstance) => (path, value) => {
+  console.log('elements', elements);
+  console.log('path', path);
+  console.log('value', value);
   switch (path) {
     case 'inputIsValid':
+      elements.feedback.classList.remove('text-danger', 'text-success');
       if (value) {
-        elements.feedback.classList.remove('text-danger');
         elements.feedback.classList.add('text-success');
         elements.feedback.textContent = i18nextInstance.t('sucsess');
       } else {
-        elements.feedback.classList.remove('text-success');
         elements.feedback.classList.add('text-danger');
         elements.feedback.textContent = i18nextInstance.t('validateErrors');
       }
       break;
     case 'inputValue':
-      elements.input.focus();
-      elements.input.value = '';
-      break;
     case 'rssUrls':
       elements.input.focus();
       elements.input.value = '';
